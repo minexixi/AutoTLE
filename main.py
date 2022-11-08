3e2ddd65ba22127e0f2643f6d4bd65f099822afc
minexixi <1263188827@qq.com>
2022-11-08 10:11:06 +0800

    Auto Update.

--- main.py
+++ main.py
@@ -43,12 +43,14 @@ Tles = [i for i in Tles if i != '']
 satelistNumber = [
     '25544U', '27607U', '43017U', '43137U', '43678U', '44909U', '22825U', '40903U', '40911U', '40907U'
     , '40910U', '07530U', '42761U', '42759U', '40908U', '24278U', '42017U', '44354U', '44881U',
-    '48274U', '43803U', '47438U', '43937U', '25338U', '28654U', '33591U', '44387U', '50466U'
+    '48274U', '43803U', '47438U', '43937U', '25338U', '28654U', '33591U', '44387U', '50466U',
+    '54216U','53106U'
 ]
 satelistName = [
-    'ISS', 'SO-50', 'AO-91(FOX-1B)', 'AO-92(FOX-1D)', 'PO-101(DIWATA-2B)', 'RS-44', 'AO-27', 'XW-2A', 'XW-2B', 'XW-2D',
+    'ISS(ZARYA)', 'SO-50', 'AO-91(FOX-1B)', 'AO-92(FOX-1D)', 'PO-101(DIWATA-2B)', 'RS-44', 'AO-27', 'XW-2A', 'XW-2B', 'XW-2D',
     'XW-2F', 'AO-7', 'CAS-4A', 'CAS-4B', 'CAS-3H(LilacSat-2)', 'FO-29', 'EO-88', 'PSAT-2(NO-104)', 'CAS-6',
-    'TIANHE-1', 'JO-97', 'UVSQ-SAT', 'FO-99', 'NOAA-15', 'NOAA-18', 'NOAA-19', 'METEOR-M2', 'XW-3(HO-113)'
+    'CSS(TianHe)', 'JO-97', 'UVSQ-SAT', 'FO-99', 'NOAA-15', 'NOAA-18', 'NOAA-19', 'METEOR-M2', 'XW-3(HO-113)'
+    ,'CSS(MengTian)','GreenCube'
 ]
 for i in range(len(satelistNumber)):
     temp = [temp for temp in Tles if satelistNumber[i] in temp]
